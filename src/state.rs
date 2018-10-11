e80785d4221e63d65f5d9a17e7974c17494d84cb
Peter East <petereast@pm.me>
2018-10-11 11:17:53 +0100

    Add definitions for system internal state

---
+++ src/state.rs
@@ -0,0 +1,25 @@
+use events::*;
+use mpsc::SyncSender;
+
+#[derive(Clone, Debug)]
+pub struct SessionState {
+    pub title: String,
+    pub description: String,
+    pub accepting_new_submissions: bool,
+    pub session_events: Vec<SystemEvents>,
+}
+
+impl SessionState {
+    pub fn new(title: String, description: String) -> Self {
+        Self {
+            title,
+            description,
+            accepting_new_submissions: true,
+            session_events: Vec::new(),
+        }
+    }
+}
+
+pub struct AppState {
+    pub outgoing_events: SyncSender<SystemEvents>,
+}
