19b24b71834c0c32e5c04838a5a4492da3c0964e
Peter East <petereast@pm.me>
2018-11-11 20:27:55 +0000

    Add Event::new() methods

--- src/events.rs
+++ src/events.rs
@@ -8,9 +8,25 @@ pub struct StartNewSessionEvent {
     pub session_description: String,
 }
 
+impl StartNewSessionEvent {
+    pub fn new(session_id: String, session_title: String, session_description: String) -> Self {
+        Self {
+            session_id,
+            session_title,
+            session_description,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct EndSessionEvent {
     pub session_id: String,
+}
+
+impl EndSessionEvent {
+    pub fn new(session_id: String) -> Self {
+        Self { session_id }
+    }
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
@@ -18,6 +34,16 @@ pub struct SubmitResponseEvent {
     pub session_id: String,
     pub name: String,
     pub value: u32,
+}
+
+impl SubmitResponseEvent {
+    pub fn new(session_id: String, name: String, value: u32) -> Self {
+        Self {
+            session_id,
+            name,
+            value,
+        }
+    }
 }
 
 #[derive(Debug, Clone)]
@@ -80,3 +106,13 @@ pub enum SystemEvents {
     GetResponseCount(GetResponseCount),
     GetSessionResult(GetSessionResult),
 }
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)] // Look out! The order matters
+pub enum _SystemEvents {
+    StartNewSessionEvent(StartNewSessionEvent),
+    SubmitResponseEvent(SubmitResponseEvent),
+    EndSessionEvent(EndSessionEvent),
+}
+
+// TODO: Find a better solution to serde untagged
